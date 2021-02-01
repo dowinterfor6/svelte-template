e17d9c50fa28902173417790254b84a387ca0a8b
Andrew Chan <dowinterfor6@gmail.com>
2021-01-31 19:03:57 -0800

    Add placeholder fonts

--- src/App.svelte
+++ src/App.svelte
@@ -12,6 +12,15 @@
     margin: 0;
   }
 
+  
+  :global(h1, h2, h3, h4, h5) {
+    font-family: "Montserrat", sans-serif;
+  }
+
+  :global(p, span, div, ul, li) {
+    font-family: "Roboto", sans-serif;
+  }
+
   main {
     height: 100%;
     padding: 0;
